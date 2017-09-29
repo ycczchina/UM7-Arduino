bdef67cf976ed394b342a486c42eb9588c41d34f
ycczchina <chenzhen@umich.edu>
2017-09-29 04:09:46 -0400

    Update and rename UM7.h to src/UM7.h

--- src/UM7.h
+++ src/UM7.h
@@ -9,6 +9,12 @@
 
 #include <stdlib.h>
 
+// UM7_CREG_MISC_SETTINGS 0x08
+#define UM7_MAG_ENABLED     (1 << 0)      // The magnetometer will be used in state updates
+#define UM7_Q_ENABLED       (1 << 1)      // The sensor will run in quaternion mode rather than Euler angle mode
+#define UM7_ZG_ENABLED      (1 << 2)      // The sensor will attempt to measure the rate gyro bias on startup
+#define UM7_PPS_ENABLED     (1 << 8)      // Use the TX2 pin on the IO expansion header as the PPS input from an external GPS module
+
 class UM7{
 public:
 	short roll, pitch, yaw, roll_rate, pitch_rate, yaw_rate;
